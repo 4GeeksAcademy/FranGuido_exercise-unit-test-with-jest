5dfd9979ba3ec4d2ecbda98c0c01e128cf2beb43
Fran Guido <114300538+francinieguido@users.noreply.github.com>
2023-06-29 03:03:02 +0000

    test (completed)

--- app.js
+++ app.js
@@ -6,5 +6,38 @@ const sum = (a, b) =>{
 // console log to see the result
 console.log(sum(7,3))
 
-// Exporting function (will be used in another file)
-module.exports = {sum};
+// currency conversion funciton
+
+// dollar to yen
+let fromDollarToYen = (USD) =>{
+    let valueYen = USD * 144.35
+    return valueYen
+     
+}
+
+console.log(fromDollarToYen(10)) // equivalent to 1443.5 Yen
+
+
+// euro to dollar
+let fromEuroToDollar = (Euro) =>{
+    let valueDollar = Euro * 1.2
+    return valueDollar
+}
+
+console.log(fromEuroToDollar(10)) // equivalent to 12 dolars
+
+
+
+// yen to pound
+
+let fromYenToPound = (Yen) =>{ 
+    let valuePound = Yen * 0.005
+    return valuePound
+}
+
+console.log(fromYenToPound(10)) // equivalent to 0.5 sterling pounds
+
+// Exporting functions (will be used in another file)
+module.exports = {sum, fromDollarToYen, fromEuroToDollar, fromYenToPound};
+
+
--- test.js
+++ test.js
@@ -1,13 +1,43 @@
-// importing 'sum' function from the app.js file
+// importing functions from the app.js file
 const { sum } = require('./app.js');
+const { fromDollarToYen } = require('./app.js');
+const { fromEuroToDollar } = require('./app.js');
+const { fromYenToPound } = require('./app.js');
 
-// start first test
-test('adds 14 + 9 to equal 23', () =>{
+// start first test (EXAMPLE)
+/*test('adds 14 + 9 to equal 23', () =>{
     //calling sum function with two numbers inside the test
     let total = sum(14, 9);
 
     // expecting the sum of those numbers to be 23
     expect(total).toBe(23);
-});
+});*/
 
 // testing currency conversion
+
+
+
+// dollar to yen test
+test('10 dollars equal 1443.5', () =>{
+    let currencyDY = fromDollarToYen(10);
+
+    //expecting 1443.5 yen
+    expect(currencyDY).toBe(1443.5);
+});
+
+
+// euro to dollar test
+test('10 euro equal 12 dollar', () =>{
+    let currencyED = fromEuroToDollar(10);
+
+    //expecting 12 dollar
+    expect(currencyED).toBe(12);
+});
+
+// yen to pound test
+test('10 yen equal 0.05 pounds', () =>{
+    let currencyYP = fromYenToPound(10);
+
+    //expecting 0.05 pound
+    expect(currencyYP).toBe(0.05);
+});
